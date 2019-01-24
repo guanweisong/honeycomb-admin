a63ac645e8abf3a69bdb94eae013728c0a0db190
guanweisong <30776162@qq.com>
2019-01-25 00:14:43 +0800

    通过环境变量设置api前缀

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { message } from 'antd';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:7001',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7001' : 'http://api.guanweisong.com',
   withCredentials: true,
   timeout: 10000,
   headers: {},
