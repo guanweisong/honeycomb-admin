30549cc71234ce6aee7e3ffb20b3130cba0e8177
guanweisong <307761682@qq.com>
2020-04-13 20:47:32 +0800

    添加GA统计

--- .umirc.js
+++ .umirc.js
@@ -4,6 +4,9 @@ export default {
   dynamicImport: {
     loading: '@/components/Loader',
   },
+  analytics: {
+    ga: 'UA-158268354-3',
+  },
   title: '管理系统',
   ignoreMomentLocale: true,
   hash: true,
